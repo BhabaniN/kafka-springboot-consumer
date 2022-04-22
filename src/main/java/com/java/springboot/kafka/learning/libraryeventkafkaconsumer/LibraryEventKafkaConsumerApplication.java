e6d3911ab59b35af0509e9cc168aecdc1e40495e
BhabaniN <mrnayak.bhabanishankar@gmail.com>
2022-04-22 12:56:50 +0530

    initial commit

---
+++ src/main/java/com/java/springboot/kafka/learning/libraryeventkafkaconsumer/LibraryEventKafkaConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.java.springboot.kafka.learning.libraryeventkafkaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryEventKafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryEventKafkaConsumerApplication.class, args);
+	}
+
+}
